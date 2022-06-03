18d5265248eb4eb7b2da4976bc430d3c40928fa8
rakshanair <101237168+rakshanair@users.noreply.github.com>
2022-06-03 11:30:28 +0530

    Add files via upload

---
+++ Pathology/src/main/java/com/zensar/path/PathologyApplication.java
@@ -0,0 +1,23 @@
+package com.zensar.path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class PathologyApplication {
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+	return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathologyApplication.class, args);
+	}
+
+}
---
+++ Pathology/src/test/java/com/zensar/path/PathologyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zensar.path;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
