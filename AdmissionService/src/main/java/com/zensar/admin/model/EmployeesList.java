208d9da74493fdd533ab8aa7560231b43acf17ce
rakshanair <101237168+rakshanair@users.noreply.github.com>
2022-06-03 11:32:54 +0530

    Add files via upload

---
+++ AdmissionService/src/main/java/com/zensar/admin/model/EmployeesList.java
@@ -0,0 +1,19 @@
+package com.zensar.admin.model;
+
+
+import java.util.List;
+
+public class EmployeesList {
+
+	public List<Employee> employees;
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	
+}
---
+++ AdmissionService/src/test/java/com/zensar/admin/AdmissionServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zensar.admin;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionsServiceMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
