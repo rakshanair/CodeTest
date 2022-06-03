9e2e28ab78290113cea545a9f918752964113713
rakshanair <101237168+rakshanair@users.noreply.github.com>
2022-06-03 11:31:24 +0530

    Add files via upload

---
+++ HRService/src/test/java/com/zensar/hr/HrServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zensar.hr;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
