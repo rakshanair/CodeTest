1b149db06515ed83b3f9ea39a614fba2fd38d614
rakshanair <101237168+rakshanair@users.noreply.github.com>
2022-06-03 11:32:02 +0530

    Add files via upload

---
+++ Discovery/src/test/java/com/zensar/discover/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.discover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
